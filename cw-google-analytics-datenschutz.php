256e42ea6b277ea452ec11b086e4d1f741581043
christianwenzl <christianwenzl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-12 08:10:47 +0000

    First realease on wordpress.org
    git-svn-id: https://plugins.svn.wordpress.org/cw-google-analytics-datenschutz/trunk@462082 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cw-google-analytics-datenschutz.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name: CW Google Analytics Datenschutz
+Plugin URI: http://www.christian-wenzl.de/wordpress-google-analytics-datenschutz
+Description: Google Analytics nach aktuellem Stand datenschutzkonform einsetzen (Deutschland, Gewährleistung ausgeschlossen). Einstellungen unter Privatsphäre zu finden.
+Version: 1.0
+Author: Christian Wenzl
+Author URI: http://www.christian-wenzl.de
+License: GPLv2
+
+*/
+
+register_uninstall_hook(__FILE__, 'cw_GoogleAnalyticsUninstall');
+function cw_GoogleAnalyticsUninstall(){
+	delete_option('cw_google_analytics_account');
+}
+
+add_action('admin_init', 'cw_GoogleAnalyticsSettings');
+
+function cw_GoogleAnalyticsSettings(){
+	register_setting('privacy', 'cw_google_analytics_account');
+	
+	add_settings_section('cw_GoogleAnalyticsSettingsID', 'Google Analytics', 'cw_GoogleAnalyticsSettingsDescription', 'privacy');
+	
+	function cw_GoogleAnalyticsSettingsDescription(){
+		echo 'Einstellungen f&uuml;r Google Analytics';
+	}
+	add_settings_field('cw_google_analytics_account', 'Account-ID', 'cw_GoogleAnalyticsAccountInput', 'privacy', 'cw_GoogleAnalyticsSettingsID');
+	function cw_GoogleAnalyticsAccountInput(){
+		$strAccount = get_option('cw_google_analytics_account');
+		echo "<input type='text' class='regular-text code' id='cw_google_analytics_account' name='cw_google_analytics_account' value='".esc_attr($strAccount)."' /> Beispiel: UA-23456789-1";
+	}
+}
+
+
+add_action('wp_footer', 'cw_GoogleAnalyticsFooter', 90);
+function cw_GoogleAnalyticsFooter(){
+	
+	$strAccount = get_option('cw_google_analytics_account');
+	if(strlen($strAccount)<=10) return;
+
+	
+	echo "
+	<!-- CW Google Analytics Datenschutz -->
+	<script>
+	   var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', '".$strAccount."']);
+	  _gaq.push(['_trackPageview']);
+	  _gaq.push(['_gat._anonymizeIp']);
+	  _gaq.push(['_trackPageLoadTime']);
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
+	<!-- CW Google Analytics Datenschutz -->";
+	
+}
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== CW Google Analytics Datenschutz ===
+Contributors: christianwenzl
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RVT9TV47T5CLS
+Tags: google analytics, datenschutz, datenschutzkonform, anonymizeIp, anonymize, anonym
+Requires at least: 3.0.0
+Tested up to: 3.2
+Stable tag: trunk
+
+Dieses Plugin stellt eine einfache Möglichkeit zur Verfügung, Google Analytics auf der Seite datenschutzkonform einzubinden. 
+
+== Description ==
+
+Nach der Installation finden Sie unter Einstellungen, Privatsphäre ein weiteres Textfeld, bei dem Sie die Account-ID von Google Analytics eintragen können.
+
+Mit der Hinterlegung dieser ID ist Analytics sofort einsatzklar. Technisch wird bei der Darstellung des Trackingcodes der Parameter "_gat._anonymizeIp" hinterlegt, der die IP-Adressen erst kürzt und anschließend an Google übermittelt.
+
+Zum datenschutzkonformen Einsatz ist weiterhin eine Vereinbarung mit Google notwendig. Den Vertrag dazu (kostenlos) und weitere, ausführliche Informationen stellt der Datenschutzbeauftragten von Hamburg zur Verfügung, zu finden unter
+http://www.christian-wenzl.de/google-analytics-datenschutzkonform
+
+== Installation ==
+
+1. ZIP-Datei über den Installer installieren
+2. Unter Privatsphäre-Einstellungen die Google Analytics Account-ID eintragen
+3. Fertig, Tracking erfolgt sofort.
+
+== Frequently Asked Questions ==
+
+= Ist diese Einbindung 100% rechtssicher? =
+
+Sie entspricht den aktuellen Vorgaben, diese können sich aber jederzeit ändern. Für die Rechtssicherheit übernehme ich keine Haftung. Bitte kontaktieren Sie im Zweifelsfall Ihren Anwalt des Vertrauens.
+
+== Screenshots ==
+
+1. Privatsphäre-Einstellung
+2. Darstellung im Quelltext
+
+== Changelog ==
+
+= 1.0 =
+* Erstveröffentlichung bei wordpress.org
